7783d9699e84e86d0afecb48b4661681ed5bdc51
Ola Zalecki <aezalecki@gmail.com>
2024-03-06 13:47:06 -0500

    large update

---
+++ procedure/code/hist.R
@@ -0,0 +1,10 @@
+web = read_csv("https://raw.githubusercontent.com/azalecki/Zalecki-2023/main/data/raw/public/WS_1_29_24.csv")
+
+hist(web$CIRC, breaks=150)
+
+plot(, mpg, main="Scatterplot Example",
+     xlab="Car Weight ", ylab="Miles Per Gallon ", pch=19)
+
+boxplot(web$VIST)
+
+hist(web$COMPSES, breaks=100)
